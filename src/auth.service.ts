f7d4f9fa2dd72dafa581b4fec29dc9adb32df89e
Kushal Gaur <501853@containerstore.com>
2024-03-17 21:13:15 +0530

    user data

--- src/auth.service.ts
+++ src/auth.service.ts
@@ -4,11 +4,13 @@ import { UserDocument } from '@app/common';
 import { JwtService } from '@nestjs/jwt';
 import { Response } from 'express';
 import { TokenPayload } from './interfaces/token-payload.interface';
+import { UsersService } from './users/users.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly configService: ConfigService,
+    private readonly usersService: UsersService,
     private readonly jwtService: JwtService,
   ) {}
 
@@ -38,7 +40,8 @@ export class AuthService {
     console.log('---------incoming token--------', jwtToken);
     console.log('---------incoming token--------', secret);
     const parsedData = this.jwtService.verify(jwtToken, secret);
-    console.log('---------parsedData--------', parsedData);
+    const userData = this.usersService.getUser({ _id: parsedData.userId });
+    console.log('---------parsedData--------', parsedData, userData);
     return parsedData;
   }
 }
